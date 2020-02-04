d1c4d2d775908e60e7eff01ccde94794cb16a22e
Michael Lowe <mrlowememphis@yahoo.com>
2020-02-03 21:04:00 -0600

    Updated reducers and Actions

--- my-app/src/actions/user.actions.js
+++ my-app/src/actions/user.actions.js
@@ -1,84 +1,80 @@
 import { axiosWithAuth } from "../../axiosWithAuth";
 
-export const USER_LOAD_START = "USER_LOAD_START";
-export const USER_LOAD_SUCCESS = "USER_LOAD_SUCCESS";
-export const USER_LOAD_FAILURE = "USER_LOAD_FAILURE";
-
+export const USER_GET_START = "USER_GET_START";
+export const USER_GET_SUCCESS = "USER_GET_SUCCESS";
+export const USER_GET_FAILURE = "USER_GET_FAILURE";
 export const USER_POST_START = "USER_POST_START";
 export const USER_POST_SUCCESS = "USER_POST_SUCCESS";
 export const USER_POST_FAILURE = "USER_POST_FAILURE";
-
 export const USER_PUT_START = "USER_PUT_START";
 export const USER_PUT_SUCCESS = "USER_PUT_SUCCESS";
 export const USER_PUT_FAILURE = "USER_PUT_FAILURE";
-
 export const USER_DELETE_START = "USER_DELETE_START";
 export const USER_DELETE_SUCCESS = "USER_DELETE_SUCCESS";
 export const USER_DELETE_FAILURE = "USER_DELETE_FAILURE";
-
-export const getUser = () => dispatch => {
-  dispatch({ type: USER_LOAD_START });
+export const getUser = (id, token) => dispatch => {
+  
+  dispatch({ type: USER_GET_START });
   axiosWithAuth()
-    .get(`/user`)
+    .get(`/users/${id}`, token)
     .then(res => {
       dispatch({
-        type: USER_LOAD_SUCCESS,
+        type: USER_GET_SUCCESS,
+        payload: res.data
+      });
+      localStorage.setItem("user", JSON.stringify(res.data));
+    })
+    .catch(err => {
+      dispatch({
+        type: USER_GET_FAILURE,
+        payload: "error getting user" + err
+      });
+    });
+};
+export const postUser = value => dispatch => {
+  dispatch({ type: USER_POST_START, payload: value });
+  return axiosWithAuth()
+    .post(`/auth/register`, value)
+    .then(res => {
+      console.log(`postUser: value: res: `, value, res);
+      
+      localStorage.setItem("userName", JSON.stringify(value.username));
+      localStorage.setItem("userEmail", JSON.stringify(value.userEmail));
+      dispatch({
+        type: USER_POST_SUCCESS,
         payload: res.data
       });
     })
     .catch(err => {
       dispatch({
-        type: USER_LOAD_FAILURE,
-        payload: "error loading projects"
+        type: USER_POST_FAILURE,
+        payload: "error posting data" + err
       });
     });
 };
-
-export const postUser = value => dispatch => {
-  // dispatch({ type: USER_POST_START, payload: value });
-  // axiosWithAuth()
-  // .post(`/projects`, value)
-  // .then(res => {
-  console.log(`user.actions: postUser: value: `, value);
-  dispatch({
-    type: USER_POST_SUCCESS,
-    payload: value
-  });
-
-  localStorage.setItem("token", "temp_token");
-  // window.location.href = "/values-selection";
-  // })
-  // .then(() => (window.location.href = "/values-selection"))
-  // .catch(err => {
-  //   dispatch({
-  //     type: USER_POST_FAILURE,
-  //     payload: "error posting data"
-  //   });
-  // });
-};
-
 export const putUser = (value, id) => dispatch => {
-    dispatch({ type: USER_PUT_START, payload: value });
-    axiosWithAuth()
-      .put(`/user/${id}`, value)
-      .then(res => {
-        dispatch({
-          type: USER_PUT_SUCCESS,
-          payload: res.data
-        });
-      })
-      .then(() => (window.location.href = "/home"))
-      .catch(err => {
-        dispatch({
-          type: USER_PUT_FAILURE,
-          payload: "error putting user data"
-        });
+  dispatch({ type: USER_PUT_START, payload: value });
+  axiosWithAuth()
+    .put(`/users/${id}`, value)
+    .then(res => {
+      dispatch({
+        type: USER_PUT_SUCCESS,
+        payload: res.data
       });
-  };
-  export const deleteUser = id => dispatch => {
-    dispatch({ type: USER_DELETE_START });
-    axiosWithAuth()
-    .delete(`/user/${id}`)
+
+      localStorage.setItem("user", JSON.stringify(res.data));
+    })
+    .catch(err => {
+      dispatch({
+        type: USER_PUT_FAILURE,
+        payload: "error putting user data" + err
+      });
+    });
+};
+export const deleteUser = id => dispatch => {
+  dispatch({ type: USER_DELETE_START });
+  axiosWithAuth()
+    .delete(`/users/${id}`)
     .then(res => {
       dispatch({
         type: USER_DELETE_SUCCESS,
@@ -89,8 +85,7 @@ export const putUser = (value, id) => dispatch => {
     .catch(err => {
       dispatch({
         type: USER_DELETE_FAILURE,
-        payload: "error deleting user data"
+        payload: "error deleting user data" + err
       });
     });
 };
-
--- my-app/src/reducers/userReducer.js
+++ my-app/src/reducers/userReducer.js
@@ -1,96 +1,97 @@
 import {
-    //     USER_LOAD_START,
-    //     USER_LOAD_SUCCESS,
-    //     USER_LOAD_FAILURE,
-    //     USER_POST_START,
-    USER_POST_SUCCESS
-    //     USER_POST_FAILURE,
-    //     USER_PUT_START,
-    //     USER_PUT_SUCCESS,
-    //     USER_PUT_FAILURE,
-    //     USER_DELETE_START,
-    //     USER_DELETE_SUCCESS,
-    //     USER_DELETE_FAILURE
-  } from "../actions/user.actions";
-  
-  import { userData } from "../../dummy-data";
-  
-  const initialState = {
-     user: null
-  };
+  USER_GET_START,
+  USER_GET_SUCCESS,
+  USER_GET_FAILURE,
+  USER_POST_START,
+  USER_POST_SUCCESS,
+  USER_POST_FAILURE,
+  USER_PUT_START,
+  USER_PUT_SUCCESS,
+  USER_PUT_FAILURE,
+  USER_DELETE_START,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAILURE
+} from "../actions/user.actions";
 
-  const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-      //   case USER_LOAD_START:
-      //     return {
-      //       ...state,
-      //       isLoading: true
-      //     };
-      //   case USER_LOAD_SUCCESS:
-      //     return {
-      //       ...state,
-      //       USER: action.payload,
-      //       isLoading: false
-      //     };
-      //   case USER_LOAD_FAILURE:
-      //     return {
-      //       ...state,
-      //       error: action.payload,
-      //       isLoading: false
-      //     };
-      //   case USER_POST_START:
-      //     return {
-      //       ...state,
-      //       isLoading: true
-      //     };
+const initialState = {
+  user: {}
+};
 
-      case USER_POST_SUCCESS:
-      console.log(`USER_POST_SUCCESS: action.payload: `, action.payload);
+const userReducer = (state = initialState, action) => {
+  console.log(`userReducer: state: `, state);
+  switch (action.type) {
+    case USER_GET_START:
       return {
         ...state,
+        isLoading: true
+      };
+    case USER_GET_SUCCESS:
+      return {
+        ...state,
+       
         user: action.payload,
         isLoading: false
       };
-    //   case USER_POST_FAILURE:
-    //     return {
-    //       ...state,
-    //       error: action.payload,
-    //       isLoading: false
-    //     };
-    //   case USER_PUT_START:
-    //     return {
-    //       ...state,
-    //       isLoading: true
-    //     };
-    //   case USER_PUT_SUCCESS:
-    //     return {
-    //       ...state,
-    //       USER: [...state.USER, action.payload.USER],
-    //       notes: [action.payload.notes]
-    //     };
-    //   case USER_PUT_FAILURE:
-    //     return {
-    //       ...state,
-    //       error: action.payload,
-    //       isLoading: false
-    //     };
-    //   case USER_DELETE_START:
-    //     return {
-    //       ...state,
-    //       isLoading: true
-    //     };
-    //   case USER_DELETE_SUCCESS:
-    //     return {
-    //       ...state,
-    //       USER: [action.payload]
-    //     };
-    //   case USER_DELETE_FAILURE:
-    //     return {
-    //       ...state,
-    //       error: action.payload,
-    //       isLoading: false
-    //     };
+    case USER_GET_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case USER_POST_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case USER_POST_SUCCESS:
+      console.log(`USER_POST_SUCCESS: action.payload: `, action.payload);
+      return {
+        user: action.payload,
+        isLoading: false
+      };
+    case USER_POST_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case USER_PUT_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case USER_PUT_SUCCESS:
+      return {
+        ...state,
+      
+        user: action.payload[0]
+      };
+    case USER_PUT_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    case USER_DELETE_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case USER_DELETE_SUCCESS:
+      return {
+        ...state,
+       
+        user: {},
+        isLoading: false
+      };
+    case USER_DELETE_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
     default:
       return state;
   }
 };
+export default userReducer;
