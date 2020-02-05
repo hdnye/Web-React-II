dfaa332952bf019e078c9b61f68025c1d93daff6
Heather Nye <heatherdnye@gmail.com>
2020-02-05 16:35:43 -0600

    private route

---
+++ my-app/src/components/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+
+
+export default function PrivateRoute({ component: Component, ...rest}) {
+     let loggedIn = localStorage.getItem('token');
+    return (
+        <Route
+         {...rest}>
+             {loggedIn ? <Component /> : <Redirect to='login' /> }
+        </Route>
+     );
+};   
+        
+ 
